ce27a830ad7b1a09547489f1b956b65cc3e32fc8
zforzed <mi6a2006@gmail.com>
2022-09-08 14:42:18 +0300

    Added buttons for choice and result score container

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-let choiceArray = ["Rock","Paper","Scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+document.getElementById("new-game-button").addEventListener("click", (event)=> {
+   document.querySelector(".pop-up").classList.add("hidden");
+   playerScore = 0;
+   computerScore = 0;
+   document.getElementById("result").innerHTML = "Press button to know who wins";
+   document.getElementById("result").removeAttribute("class");
+   document.getElementById("player-score").innerHTML=0;
+   document.getElementById("computer-score").innerHTML=0;
+});
+
+document.querySelectorAll("button").forEach(button=>{
+   button.addEventListener("click", (event)=>{
+      playRound(event.target.innerHTML);
+   });
+});
 
 function getComputerChoice() {
+   let choiceArray = ["Rock","Paper","Scissors"];
    let i = Math.floor(Math.random()*3);
-   let computerSelection = choiceArray[i];
-   return computerSelection;
+   let computerChoice = choiceArray[i];
+   return computerChoice;
 }
-function inputCorrection(input) {
-   let corrected = input[0].toUpperCase() + input.slice(1).toLowerCase();
-   return corrected;
+function checkWinner(player, computer) {
+   if (player === 5 || computer === 5) {
+      document.querySelector(".pop-up").classList.remove("hidden");
+      document.getElementById("finished-game").innerHTML = (player === 5? "Player":"Computer") + " wins!";
+   }
 }
+
 function nobodyWins(a,b) {
    document.getElementById("computerChoice").innerHTML = "Computer choice is: " + b;
    document.getElementById("result").innerHTML = "Nobody wins. " + a + " equals to " + b;   
@@ -20,39 +41,44 @@ function playerWins(a,b) {
    document.getElementById("result").innerHTML = "You Win! " + a + " beats " + b;
    document.getElementById("result").classList.remove("lose");
    document.getElementById("result").classList.add("win");
+   playerScore++;
+   document.getElementById("player-score").innerHTML = playerScore;
+   checkWinner(playerScore, computerScore);
 }
 function playerLose(a,b) {
    document.getElementById("computerChoice").innerHTML = "Computer choice is: " + b;
    document.getElementById("result").innerHTML = "You Lose. " + b + " beats " + a;
    document.getElementById("result").classList.remove("win");
    document.getElementById("result").classList.add("lose");
+   computerScore++;
+   document.getElementById("computer-score").innerHTML = computerScore;
+   checkWinner(playerScore, computerScore);
+
 }
-function whoWins() {
-   let playerInput = document.getElementById("playerInput").value;
-   let playerSelection = inputCorrection(playerInput);
-   document.getElementById("playerInput").value = playerSelection;
-   let computerSelection = getComputerChoice();
-   switch(playerSelection) {
+
+function playRound(playerChoice) {
+   let computerChoice = getComputerChoice();
+   switch(playerChoice) {
       case "Rock":
-         if (computerSelection === "Rock") {
-            nobodyWins(playerSelection, computerSelection);
-         } else if (computerSelection === "Paper") {
-            playerLose(playerSelection, computerSelection);
-         } else playerWins(playerSelection, computerSelection);
+         if (computerChoice === "Rock") {
+            nobodyWins(playerChoice, computerChoice);
+         } else if (computerChoice === "Paper") {
+            playerLose(playerChoice, computerChoice);
+         } else playerWins(playerChoice, computerChoice);
          break;
       case "Paper":
-         if (computerSelection === "Paper") {
-            nobodyWins(playerSelection, computerSelection);
-         } else if (computerSelection === "Scissors") {
-            playerLose(playerSelection, computerSelection);
-         } else playerWins(playerSelection, computerSelection);
+         if (computerChoice === "Paper") {
+            nobodyWins(playerChoice, computerChoice);
+         } else if (computerChoice === "Scissors") {
+            playerLose(playerChoice, computerChoice);
+         } else playerWins(playerChoice, computerChoice);
          break;
       case "Scissors":
-         if (computerSelection === "Scissors") {
-            nobodyWins(playerSelection, computerSelection);
-         } else if (computerSelection === "Rock") {
-            playerLose(playerSelection, computerSelection);
-         } else playerWins(playerSelection, computerSelection);
+         if (computerChoice === "Scissors") {
+            nobodyWins(playerChoice, computerChoice);
+         } else if (computerChoice === "Rock") {
+            playerLose(playerChoice, computerChoice);
+         } else playerWins(playerChoice, computerChoice);
          break;
    }
 }
